3e8388a54e7bb716b13aa8597e2032cb777d3988
Cholnapas <Cholnapasbow@gmail.com>
2026-07-15 10:53:08 +0700

    add Student id

--- Week4/foodcourt_02_gather.py
+++ Week4/foodcourt_02_gather.py
@@ -1,6 +1,26 @@
 # foodcourt_02_gather.py
 import asyncio
-from time import ctime
-from food_utils import place_order
+from time import time, ctime
+from food_utils import send_order_to_kitchen
 
-STUDENT_ID = "6710301051"
+async def main():
+    MY_STUDENT_ID = "6710301051"
+    print(f"{ctime()} | --- [Task 2] Practice using gather to wait for all group orders ---")
+    start_time = time()
+
+    # 1. Use asyncio.gather to place multiple orders concurrently.
+    # Each order is represented by a coroutine returned by place_order_to_kitchen.
+    t1 = asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "hainanese_chicken", "Chicken Rice"))    
+    t2 = asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "noodle", "Wonton Noodles"))
+    t3 = asyncio.create_task(send_order_to_kitchen(MY_STUDENT_ID, "steak", "Sizzling Steak"))
+
+    results = await asyncio.gather(t1, t2, t3)
+
+    # 2. Print the results of all orders once they are completed.
+    for dish in results:
+        print(f"{ctime()} | [Pickup] Shop: {dish['shop']} | Menu: {dish['menu']} is ready!")
+
+    print(f"{ctime()} | Total time: {time() - start_time:.2f} seconds (Equals to the slowest dish).")
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- Week4/foodcourt_04_wait_for.py
+++ Week4/foodcourt_04_wait_for.py
@@ -1,1 +1,26 @@
 # foodcourt_04_wait_for.py
+import asyncio
+from time import ctime
+from food_utils import send_order_to_kitchen
+
+async def main():
+    MY_STUDENT_ID = "6710301051"
+    print(f"{ctime()} | --- [Task 4] Practice using wait_for to queue multiple orders ---")
+
+    try:
+        # 1. Order a steak (takes4) Order
+        print(f"{ctime()} | [System] Order sent. Monitoring 2.0s timeout limit...")
+
+        result = await asyncio.wait_for(
+            send_order_to_kitchen(MY_STUDENT_ID, "steak", "T-bone Steak"), 
+            timeout=2.0
+        )
+
+        print(f"{ctime()} | Success: {result}")
+
+    except asyncio.TimeoutError:
+        print(f"{ctime()} | Timeout occured: Steak took too long! Leaving the food court now.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
