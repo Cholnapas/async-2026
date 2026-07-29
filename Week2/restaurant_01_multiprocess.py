28a0316ebc44af67a182c0e939301df5a9b5131d
Cholnapas <Cholnapasbow@gmail.com>
2026-07-29 18:37:55 +0700

    Addres

--- Week2/restaurant_01_multiprocess.py
+++ Week2/restaurant_01_multiprocess.py
@@ -1,2 +1,43 @@
 import asyncio
+from time import sleep, ctime, time
 import multiprocessing
+
+def greet_diner(customer):
+    print(f"{ctime()} -> Greeting for Customer - {customer}...")
+    sleep(1)
+    print(f"{ctime()} -> Greeting for Customer -{customer}...Done!")
+
+def customer_private_workflow(customer):
+    print(f"{ctime()} [Process-{customer}] Taking Order...")
+    sleep(1)
+    print(f"{ctime()} [Process-{customer}] Taking Order...Done!")
+
+    print(f"{ctime()} [Process-{customer}] Cooking spaghetti...")
+    sleep(1)
+    print(f"{ctime()} [Process-{customer}] Cooking spaghetti...Done!")
+
+    print(f"{ctime()} [Process-{customer}] Manage Bar for Drink...")
+    sleep(1)
+    print(f"{ctime()} [Process-{customer}] Manage Bar for Drink...Done!")
+    print(f"{ctime()} [Process-{customer}] All served!\n")
+
+if __name__ == "__main__":
+    customers = ["A", "B", "C"]
+    start_time = time()
+
+    for customer in customers:
+        greet_diner(customer)
+
+    print(f"\n{ctime()} == All Customers greeted. Splitting into individual processes ===\n")
+
+    customer_processes = []
+    for customer in customers:
+        p = multiprocessing.Process(target=customer_private_workflow, args=(customer,))
+        customer_processes.append(p)
+        p.start()
+
+    for p in customer_processes:
+        p.join()
+
+    duration = time() - start_time
+    print(f"{ctime()} Finished cooking in {duration:.2f} seconds")
